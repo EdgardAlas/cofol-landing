4ff437cd5dc6e217f125bcf79691af522c827bd4
Edgard Alas <edgardalasu@gmail.com>
2026-04-23 14:27:37 -0600

    feat: add sitemap integration and link in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 const env = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
   site: env.SITE_URL,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
