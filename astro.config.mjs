e052eeefe411319081cf999dfdc95350872368a6
Edgard Alas <edgardalasu@gmail.com>
2026-04-23 13:27:48 -0600

    fix: SEO and image accessibility
    - Add SEO properties
    - Add width and height attributes to philosophy image for better accessibility and performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
+const env = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
+  site: env.SITE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
